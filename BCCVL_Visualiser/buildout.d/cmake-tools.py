1956e60229b2058791a6cabbaaafb23298502e44
Daniel Baird <daniel@danielbaird.com>
2017-08-08 16:34:18 +1000

    getting cmake working for mapserver

--- BCCVL_Visualiser/buildout.d/cmake-tools.py
+++ BCCVL_Visualiser/buildout.d/cmake-tools.py
@@ -1,6 +1,6 @@
 
 # helpers for building mapserver by cmake
-
+import os
 
 def make_build_dir(options, buildout_dir, env):
 	''' make the build dir that cmake insists on '''
@@ -10,6 +10,9 @@ def make_build_dir(options, buildout_dir, env):
 	print(buildout_dir)
 	print('env')
 	print(env)
+
+	# make a build dir in the compile directory
+	os.mkdir(os.path.join(options['compile-directory'], 'build'))
 
 
 
