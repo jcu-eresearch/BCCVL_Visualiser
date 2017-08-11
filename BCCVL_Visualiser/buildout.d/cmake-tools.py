8461dfda3bc3d2f1c1bd17f4510f02a88b090f62
Daniel Baird <daniel@danielbaird.com>
2017-08-11 16:17:40 +1000

    trying mapserver v6

--- BCCVL_Visualiser/buildout.d/cmake-tools.py
+++ BCCVL_Visualiser/buildout.d/cmake-tools.py
@@ -12,6 +12,7 @@ def make_build_dir(options, buildout_dir, env):
 	print(env)
 
 	# make a build dir in the compile directory
+	
 	os.mkdir(os.path.join(options['compile-directory'], 'mapserver-' + options['version'], 'build'))
 
 
